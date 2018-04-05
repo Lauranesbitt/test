89d9c66e73f9acc52a0c34ea09841e21b00028c7
Lauranesbitt <lauraamynesbitt@gmail.com>
2018-04-05 18:24:43 +0100

    added some changes to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
    devise_for :users
+   devise_scope :user do
+  authenticated :user do
+    root 'patients#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
 resources :requests
  resources :clinics
  resources :hospitals
@@ -14,4 +23,6 @@ resources :requests
  get 'reviews/edit'
 
  resources :patients
+
+
 end
